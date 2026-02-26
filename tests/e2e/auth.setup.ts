dedf85acb588bf556ef4c729589666a9f7240163
Tobias Kampmann <tobias.kampmann@udo.edu>
2026-02-26 23:14:44 +0100

    dont wait

--- tests/e2e/auth.setup.ts
+++ tests/e2e/auth.setup.ts
@@ -4,12 +4,12 @@ import pathToAuthStorage from './auth.path';
 setup('authenticate', async ({ page }) => {
     await page.goto('/');
     await page.click('text=Login');
-    await page.waitForLoadState('networkidle');
+    await page.waitForLoadState('domcontentloaded');
 
     await page.fill('input[name="username"], input[id="username"]', 'adminuser');
     await page.fill('input[name="password"], input[id="password"]', 'adminpassword');
     await page.click('button[type="submit"], button:has-text("Sign"), button:has-text("Login")');
-    await page.waitForLoadState('networkidle');
+    await page.waitForLoadState('domcontentloaded');
 
     await page.context().storageState({ path: pathToAuthStorage });
 });
