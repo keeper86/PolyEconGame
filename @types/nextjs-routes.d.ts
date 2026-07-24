7ee2e8b677b7080e42a72f0da56c97825d4cb4ea
Tobias Kampmann <tobias.kampmann@udo.edu>
2026-07-24 02:21:31 +0200

    logo-categorizer

--- @types/nextjs-routes.d.ts
+++ @types/nextjs-routes.d.ts
@@ -13,6 +13,8 @@ declare module "nextjs-routes" {
   export type Route =
     | StaticRoute<"/">
     | StaticRoute<"/account">
+    | StaticRoute<"/admin/categorize-logos">
+    | StaticRoute<"/api/admin/categorize-logos">
     | DynamicRoute<"/api/auth/[...nextauth]", { "nextauth": string[] }>
     | StaticRoute<"/api/ping">
     | DynamicRoute<"/api/public/[...trpc]", { "trpc": string[] }>
